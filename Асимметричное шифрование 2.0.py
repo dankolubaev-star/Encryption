b2aa3cd32ee5a5dd2db6e313e639da8b5c217d38
dankolubaev-star <dankolubaev@gmail.com>
2025-09-05 19:16:21 +0300

    Update Асимметричное шифрование 2.0.py

--- Асимметричное шифрование 2.0.py
+++ Асимметричное шифрование 2.0.py
@@ -27,4 +27,45 @@ def _nextprime(x):
 def _inverse(a, m):
     x0, x1 = 1, 0
     r0, r1 = a, m 
-while r1 !=
+while r1 != 0:
+    q = r0 // r1
+         old_r0, old_r1 = r0, r1
+         old_x0, old_x1 = x0, x1
+         r0 = old_r1
+         r1 = old_r0 - q * old_r1
+         x0 = old_x1
+         x1 = old_x0 - q * old_x1
+if r0 != 1:
+    raise ValueError("Ошибка, нет обратного по молудю")
+return x0 % m
+
+def rsa_keygen(p_seed=7, q_seed=11, e=3):
+    p = _nextprime(p_seed) 
+    q = _nextprime(q_seed)
+    if p == q:
+        q = _nextprime(q + 2)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    if gcd(e, phi) != 1:
+        e = 3
+        while gcd(e, phi) != 1:
+            e += 2
+    d = inverse(e, phi) 
+    return (e, n), (d, n)
+
+def encrypt(message, public_key):
+    e, n = public_key
+    message_bytes = message.encode()
+    m = int.from_bytes(message_bytes, "big")
+    if m >= n:
+        raise ValueError("Много символом")
+    c = pow(m, e, n)
+    return c
+
+def decrypt(ciphertext, private_key):
+    d, n = private_key
+    m = pow(ciphertext, d, n)
+    message_bytes = m.to_bytes((m.bit_length() + 7) // 8, "big")
+    message = message_bytes.decode()
+    return message
+       
